2ec65c7c1240c5f38afdf26453316fd313844537
Aavash Kharel <aavash.kh@gmail.com>
2026-09-06 20:40:21 -0500

    Initial commit setup client and server side

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Health Check
+app.get('/api/health', (req, res) => {
+    res.join({status : 'ok', message : 'Server Running'});
+});
+
+// Database Connection
+const MONGO_URI = process.env.MONGO_URI;
+if (!MONGO_URI) {
+    console.log('Error : MONGO_URI is not defined in environment variables.');
+    process.exit(1);
+}
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((error) => console.error('Error connecting to MongoDB :', error));
+
+app.listen(PORT, () => console.log('Server active on port ${PORT}'));
